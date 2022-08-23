7d00afef086dea8479d3afe95201846051dd1dbc
nnkn <nnkn2x@gmail.com>
2022-08-23 13:33:10 +0800

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,20 +1,11 @@
 module.exports = {
   pathPrefix: '/running_page', // Change to `/running_page` when running on github pages
   siteMetadata: {
-    siteTitle: 'Running Page',
-    siteUrl: 'https://yihong.run',
+    siteTitle: 'NNKN的跑步记录',
+    siteUrl: 'https://running-page-rho-one.vercel.app/',
     logo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTtc69JxHNcmN1ETpMUX4dozAgAN6iPjWalQ&usqp=CAU',
     description: 'Personal site and blog',
-    navLinks: [
-      {
-        name: 'Blog',
-        url: 'https://yihong.run/running',
-      },
-      {
-        name: 'About',
-        url: 'https://github.com/yihong0618/running_page/blob/master/README-CN.md',
-      },
-    ],
+    navLinks: [ ],
   },
   plugins: [
     'gatsby-transformer-json',
